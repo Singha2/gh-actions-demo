a2a16c86605d83061efbc4f72c4c4abe3b8eb389
Amit Singh <amit.pccs@gmail.com>
2025-01-04 21:24:04 +0530

    adding github actions workflow

--- src/main/java/com/dikea/gh/actions/HelloController.java
+++ src/main/java/com/dikea/gh/actions/HelloController.java
@@ -1,5 +1,6 @@
 package com.dikea.gh.actions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
     
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from SpringBoot!");
+    }
+
+    @GetMapping("/profile")
+    public String getActiveProfile() {
+        return "Active Profile: " + activeProfile;
     }
 }
